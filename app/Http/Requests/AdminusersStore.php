6ff250b508046c47e5371993edf736507cd740c8
WJING1 <2692548781@qq.com>
2019-08-05 14:29:07 +0800

    管理员wangjing

--- app/Http/Requests/AdminusersStore.php
+++ app/Http/Requests/AdminusersStore.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AdminuserStore extends FormRequest
+class AdminusersStore extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,13 +24,12 @@ class AdminuserStore extends FormRequest
     public function rules()
     {
         return [
-            //
-            'uname' => 'required|regex:/^[a-zA-Z]{1}[\w]{7,15}$/|unique:users',
+            //验证规则
+            'uname' => 'required|regex:/^[a-zA-Z]{1}[\w]{7,15}$/|unique:admin_users',
             'upass' => 'required|regex:/^[\w]{6,18}$/',
-            'repass' => 'required|same:upass',
-            
         ];
     }
+
     //验证提示
     public function messages(){
         return [
@@ -38,10 +37,7 @@ class AdminuserStore extends FormRequest
             'uname.required'=>'用户名必填',    
             'uname.regex'=>'用户名格式错误',    
             'upass.required'=>'密码必填',    
-            'upass.regex'=>'密码格式错误',    
-            'repass.required'=>'确认密码必填',    
-            'repass.same'=>'俩次密码不一致',    
-            
+            'upass.regex'=>'密码格式错误',   
         ];
     }
 }
---
+++ resources/views/admin/adminuser/edit.blade.php
@@ -0,0 +1,81 @@
+@extends('admin.layout.index')
+
+@section('content')
+        {{-- 显示验证错误开始 --}}
+@if (count($errors) > 0)
+<div class="alert alert-error">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    {{-- <strong>哦，快！</strong> 更改一些内容并尝试再次提交。 --}}
+
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+{{-- 显示验证错误结束 --}}
+
+<div class="box span12">
+<div class="box-header" data-original-title="">
+    <h2><i class="halflings-icon white edit"></i><span class="break"></span>管理员修改</h2>
+    <div class="box-icon">
+        <a href="#" class="btn-setting"><i class="halflings-icon white wrench"></i></a>
+        <a href="#" class="btn-minimize"><i class="halflings-icon white chevron-up"></i></a>
+        <a href="#" class="btn-close"><i class="halflings-icon white remove"></i></a>
+    </div>
+</div>
+<div class="box-content">
+    <form class="form-horizontal" action="/admin/adminuser/{{ $adminusers->id }}" method="post" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+      <fieldset>
+        <div class="control-group">
+          <label class="control-label" for="typeahead">管理员名称 </label>
+          <div class="controls">
+            <input type="text" name="uname" value="{{ $adminusers->uname }}" class="span6 typeahead" id="typeahead" data-provide="typeahead" data-items="4" data-source="[&quot;Alabama&quot;,&quot;Alaska&quot;,&quot;Arizona&quot;,&quot;Arkansas&quot;,&quot;California&quot;,&quot;Colorado&quot;,&quot;Connecticut&quot;,&quot;Delaware&quot;,&quot;Florida&quot;,&quot;Georgia&quot;,&quot;Hawaii&quot;,&quot;Idaho&quot;,&quot;Illinois&quot;,&quot;Indiana&quot;,&quot;Iowa&quot;,&quot;Kansas&quot;,&quot;Kentucky&quot;,&quot;Louisiana&quot;,&quot;Maine&quot;,&quot;Maryland&quot;,&quot;Massachusetts&quot;,&quot;Michigan&quot;,&quot;Minnesota&quot;,&quot;Mississippi&quot;,&quot;Missouri&quot;,&quot;Montana&quot;,&quot;Nebraska&quot;,&quot;Nevada&quot;,&quot;New Hampshire&quot;,&quot;New Jersey&quot;,&quot;New Mexico&quot;,&quot;New York&quot;,&quot;North Dakota&quot;,&quot;North Carolina&quot;,&quot;Ohio&quot;,&quot;Oklahoma&quot;,&quot;Oregon&quot;,&quot;Pennsylvania&quot;,&quot;Rhode Island&quot;,&quot;South Carolina&quot;,&quot;South Dakota&quot;,&quot;Tennessee&quot;,&quot;Texas&quot;,&quot;Utah&quot;,&quot;Vermont&quot;,&quot;Virginia&quot;,&quot;Washington&quot;,&quot;West Virginia&quot;,&quot;Wisconsin&quot;,&quot;Wyoming&quot;]">
+          </div>
+        </div>
+
+        <div>
+            <img src="/uploads/{{ $adminusers->profile }}" alt="" style="width:80px;height:80px;border-radius:8px;margin-left:90px;">
+        </div>
+
+        <div class="control-group">
+            <label class="control-label" for="fileInput">头像</label>
+            
+            <div class="controls">
+              <div class="uploader hover" id="uniform-fileInput">
+                  <input class="input-file uniform_on" name="profile" value="" id="fileInput" type="file">
+                  <span class="filename" style="user-select: none;">No file selected</span>
+                  <span class="action" style="user-select: none;">Choose File</span>
+              </div>
+            </div>
+        </div>
+
+        <div class="control-group">
+            <label class="control-label">角色</label>
+            <div class="controls">
+                @foreach ($roles as $k=>$v)
+                    <label class="checkbox inline">
+                        <input type="radio" id="inlineCheckbox1" name="rname" value="{{ $v->id }}"> {{ $v->rname }}
+                    </label>
+                @endforeach
+                
+           
+             
+            </div>
+          </div>
+  
+        <div class="form-actions">
+          <button type="submit" class="btn btn-primary">Save changes</button>
+          <a href="/admin/adminuser">
+              <button type="" class="btn">Cancel</button>
+          </a>
+        </div>
+      </fieldset>
+    </form>   
+
+</div>
+</div>
+@endsection
